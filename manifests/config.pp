e2559c870956d42aeb8823c20a6b20d56dbe91a2
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-02 20:34:59 +0200

    Adding pxelinux.cfg configs

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,19 +1,12 @@
 class pxelinux::config inherits pxelinux {
 
-  file {
-
-    '/tftpboot/pxelinux.cfg':
-      ensure  => directory,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      require => Package['syslinux-tftpboot'];
-
-    '/tftpboot/pxelinux.cfg/default':
-      ensure  => file,
-      content => template("${module_name}/default.erb"),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644';
+  file { '/tftpboot/pxelinux.cfg':
+    ensure  => directory,
+    source  => "puppet:///${module_name}/pxelinux.cfg",
+    recurse => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['syslinux-tftpboot'];
   }
 }
